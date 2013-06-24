619d4d4da5b59f39e3b6e99c856a60a19ffb503e
Sri <sri@siscape.com>
2013-06-24 04:18:11 -0400

    added cost description

--- grails-app/domain/com/siscape/estimator/RepairCost.groovy
+++ grails-app/domain/com/siscape/estimator/RepairCost.groovy
@@ -3,6 +3,7 @@ package com.siscape.estimator
 class RepairCost {
     String costName
     String costCategory
+    String description
     BigDecimal materialCost
     BigDecimal laborCost
     BigDecimal unit
@@ -11,6 +12,7 @@ class RepairCost {
         costName ()
         costCategory inList: ["Roofing", "Exterior", "Interior", "Landscape", "Electrical",
                 "HVAC", "Kitchen", "Bath", "Flooring", "Plumbing", "Structural", "HAZMAT", "Misc"]
+        description nullable: true
         materialCost nullable: true, scale: 2
         laborCost nullable: true, scale: 2
         unit nullable: true
